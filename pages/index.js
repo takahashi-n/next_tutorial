dc2d2e6c23d74769864e9519aafbb30e1464adb1
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-04-24 17:50:56 +0900

    router

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Layout from '../components/MyLayout';
+import Link from 'next/link';
+
+const PostLink  = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
 export default () => (
-  <div>
     <Layout>
-      <p>Hello Next.js</p>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Hello Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
     </Layout>
-  </div>
 )
